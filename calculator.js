d1967e249335b6403f5f76afed44adaa63da786f
Afshin Mokhtari <afshinator@hotmail.com>
2014-06-19 01:03:09 -0700

    split functionality into singletons

--- calculator.js
+++ calculator.js
@@ -2,14 +2,17 @@ $(function() {
 
 	var displayer = function() {				// The calculators main display (shows number presses & results)
 		var $el = $( '#display' );
-		var sProgress = '';						// The ongoing calculations between each clear/reset.
+		var sProgress;							// The ongoing calculations between each clear/reset to show.
+
+		var reset = function() { sProgress = ''; };
 
 		var showNumber = function( aNumber ) {
 			$el.text( aNumber );
 		};
 
 		return {
-			showNumber : showNumber
+			reset : reset,
+			show : showNumber
 		};
 	}();
 
@@ -21,11 +24,13 @@ $(function() {
 		$mouseInput.click( function() {
 			lastCalcKeyPress = $(this).attr('id') + '';   // cast to string
 			console.log ( "click: " + lastCalcKeyPress );
+			calc.press ( lastCalcKeyPress );
 		});
 
 		$( document ).keypress( function( event ) {
 			lastCalcKeyPress = String.fromCharCode( event.which );
 			console.log ( "keybaord: " + String.fromCharCode( event.which ) );
+			calc.press ( lastCalcKeyPress );
 		});
 
 		return {
@@ -34,90 +39,94 @@ $(function() {
 
 	}();
 
-
-
-
-
-/*
-
-
-		var operators = [ '+', '-', '*', '/' ];
-		var result;
-		var started = false;
-		var dirtyResult;
-		var withinNumber;
-		var number;
-
+	var calc = function() {
+		var result;										// Ongoing result of calculations
+		var started;									// Whether the first number has been entered yet
+		var withinNumber;								// Whether the last key pressed was a number & so is this key press
+		var newNumber;
+		var isFirstNumber;								// Whether the last number entered was the first in a possible ongoing series of calculations.
+		var lastOperator;								// Stores the last operation to eval before moving on to new one
 
 		var reset = function() {
 			result = 0;
 			started = false;
-			dirtyResult = false;
 			withinNumber = false;
-			number = 0;
+			newNumber = 0;
+			isFirstNumber = true;
+			lastOperator = '';
+			displayer.reset();
+			displayer.show( result );
+		};
+
+
+		var handleNumberPress = function( whichBtn ) {
+			if ( ! withinNumber ) {				// If its the first digit of a new number
+				newNumber = whichBtn * 1;		// store it, cast it to Integer trick
+				started = true;			// 
+			} else {							// Not the first digit of a number
+				newNumber = newNumber * 10;
+				newNumber += ( whichBtn * 1 );
+			}
+
+			withinNumber = true;
+			displayer.show( newNumber );
+		};
+
+
+		var handleOperatorPress = function( whichBtn ) {
+			withinNumber = false;
+
+			if ( started ) {
+				if ( isFirstNumber ) {				// If the last number we got was the first after a reset
+					result = newNumber;
+					isFirstNumber = false;
+					lastOperator = whichBtn;
+				}
+				else {
+					if ( lastOperator === '+' ) {
+						result = result + newNumber;
+					}
+					else if ( lastOperator === '-' ) {
+						result = result - newNumber;
+					}
+					else if ( lastOperator === '*' ) {
+						result = result * newNumber;
+					}
+					else if ( lastOperator === '/' ) {
+						result = result / newNumber;
+					}
+
+					lastOperator = whichBtn;
+				}
+				displayer.show( result );
+			}
+		};
+
+
+		var handleCalculatorButtonPress = function ( whichBtn ) {
+			var operators = [ '+', '-', '*', '/' ];
+			var whichOperator;
+
+			if ( whichBtn === 'CA' ) {					// Reset any calculation in progress
+				reset();
+			} else {
+				whichOperator = operators.indexOf( whichBtn );
+				if ( whichOperator > -1 ) {				// An operator was pressed;
+					handleOperatorPress( whichBtn );
+				}
+				else {									// A number was pressed;
+					handleNumberPress( whichBtn );
+				}
+			}
+
 		};
 
 		reset();
 
-		$( 'area' ).click( function() { 
-		    var id = $(this).attr('id'); 
-		    var whichOperator;
+		return {
+			press : handleCalculatorButtonPress
+		};
 
-		    console.log( "-->" + id ); 
-
-
-		    if ( id === 'CA' ) {						// Reset any calculation in progress
-		    	reset();
-		    	$display.text ( result );
-		    } else {
-		    	whichOperator = operators.indexOf( id );
-	    		if ( whichOperator > -1 ) {				// An operator was pressed; 
-		    		console.log( "___>" + operators[whichOperator] );  
-					console.log( ' result befoer ' + result ) ; 
-		    		if ( started ) {
-						console.log('in started block');
-						if ( ! dirtyResult ) {
-							result = number;
-						}
-							if ( id === '+' ) {
-								result += number;
-						}
-						else if ( id === '-' ) {
-							result -= number;
-						}
-						else if ( id === '*' ) {
-							result = result * number;
-						}
-						else if ( id === '/' ) {
-							result = result / number;
-						}
-
-						dirtyResult = true;
-					}
-
-					withinNumber = false;
-					console.log( ' result after ' + result ) ;
-					$display.text ( result );
-				} else {								// A number was pressed
-					if ( ! withinNumber ) {				// If its the first digit of a new number
-						number = id * 1;		// store it, cast it to Integer trick
-						started = true;			// 
-					} else {							// Not the first digit of a number
-						number = number * 10;
-						number += ( id * 1 );
-					}
-
-					withinNumber = true;
-
-					$display.text( number );
-				}
-			}
-
-
-			return false; 
-		});
-
-
-*/
+	}();
 
 });
